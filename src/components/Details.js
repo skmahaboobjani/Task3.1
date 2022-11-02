9c031cb3807ebd0068f7eaa2bdd701cc7a524150
mahaboob jani <skmabujani521@gmail.com>
2022-11-02 17:12:14 +0530

    jani

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './hello.css'
+import { TfiArrowLeft,TfiArrowRight } from "react-icons/tfi";
+import {GrRefresh} from "react-icons/gr"
+import{BsStar} from "react-icons/bs"
  function Details() {
   return (
     
     <div className='mabu'>
-        <title>mabu</title>
+   
+      <hr/><br/>
+    < TfiArrowLeft/> <TfiArrowRight id='arrow'/> <GrRefresh id='re'/> 
+         <input type={'text'}  placeholder={'127.0.0.1:500/index.html'}   /> {<BsStar id='s'/>}
+        
+        <hr/>
+        
       <h1>Bitto Aggarwal</h1>
       <h6>Software Developer Educator at Geekster</h6>
       <a href="https://www.w3schools.com/">Linkidin  Github</a>
